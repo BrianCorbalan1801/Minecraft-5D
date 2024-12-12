a62867cf9b951dc0b25bad2794801b768b3978f5
benjamin martinez <benjamartinez454gmail.com>
2024-12-12 11:16:35 -0300

    aprobreee

--- Creacion de Tablas/Estructura.sql
+++ Creacion de Tablas/Estructura.sql
@@ -15,3 +15,43 @@ CREATE TABLE Estructura (
 
 SELECT m.nombre_bioma from Estructura e 
 join Bioma m on m.id = e.biomas_ID
+
+---new---
+SELECT J.jugador_id ,A.nombre_armadura 
+FROM Armadura A
+LEFT JOIN Jugador J
+ON J.armadura_ID = A.ID
+UNION
+SELECT J.jugador_id , A.nombre_armadura 
+FROM Jugador J
+LEFT JOIN Armadura A
+ON J.armadura_ID = A.ID
+
+
+
+---new---
+SELECT 
+    A.nombre_armadura AS armadura,
+    (SELECT COUNT(*) 
+     FROM Jugador J 
+     WHERE J.armadura_ID = A.ID) AS cantidad_jugadores
+FROM 
+    Armadura A;
+
+
+
+---new---
+SELECT J.jugador_id ,J.nombre_usuario ,A.nombre_armadura ,J.fecha_registro
+FROM Armadura A
+LEFT JOIN Jugador J
+ON J.armadura_ID = A.ID
+WHERE J.jugador_id <= 5
+ORDER BY J.fecha_registro;
+
+---new---
+DELETE FROM Armadura
+WHERE ID NOT IN (
+    SELECT DISTINCT armadura_ID
+    FROM Jugador
+    WHERE armadura_ID IS NOT NULL
+);
--- Insertar Datos/Jugador.sql
+++ Insertar Datos/Jugador.sql
@@ -6,3 +6,7 @@ INSERT INTO Jugador (nombre_usuario, ataque, fecha_registro, salud)
     ("iPanditaC_", 0.5, "16 de Mayo de 2009", 20),
     ("Mico0102", 0.5, "16 de Mayo de 2009", 20);
 
+---new--
+
+INSERT INTO Jugador (nombre_usuario, ataque, fecha_registro, salud) 
+VALUES ("dany", 0.5, "11 de Abril de 2012", 20), ("tomy", 0.5, "11 de Abril de 2011", 20);
